2a158394b27d6d963c8a93b0d97b84c443a7eb96
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-14 00:00:13 +0300

    add user schema field properties to user model

---
+++ models/User.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  isAdmin: { type: boolean, default: false },
+  
+}, {timestamp: true});
+
+export default mongoose.model("User", UserSchema)
