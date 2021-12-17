343ee6a6baf9e9f13ef14512f8384a28c9016f64
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-17 15:17:24 +0300

    add PUT method in user route, add verifyToken function

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,24 @@
-import express from 'express'
+import express from "express";
+import { verifyTokenAndAuthorization } from "./verifyToken.js";
+import User from '../models/User.js'
 
-const router = express.Router()
+const router = express.Router();
 
+router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+ if(req.body.password) {
+     req.body.password = CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString();
+     }
+ try {
+     const updatedUser = await User.findByIdAndUpdate(req.params.id, {
+         $set: req.body
+     }, {new:true})
+     res.status(200).json(updatedUser)
+ } catch(err){
+  res.status(500).json(err)
+ }
+});
 
-
-
-export default router
+export default router;
