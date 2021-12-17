adda1fa68405a2cc4edb357391b7427d0b2212b3
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-17 15:24:09 +0300

    add delete method

--- routes/user.js
+++ routes/user.js
@@ -4,6 +4,7 @@ import User from '../models/User.js'
 
 const router = express.Router();
 
+//update
 router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
  if(req.body.password) {
      req.body.password = CryptoJS.AES.encrypt(
@@ -20,5 +21,16 @@ router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
   res.status(500).json(err)
  }
 });
+
+//delete 
+
+router.delete('/:id', verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted...")
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
 
 export default router;
