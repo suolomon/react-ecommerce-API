fa7adb722216fb1b7254be4b63b6c126ed90684e
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-17 13:48:20 +0300

    add login route function

--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,7 @@ import CryptoJS from "crypto-js";
 
 const router = express.Router();
 
+//register
 router.post("/register", async (req, res) => {
   const newUser = new User({
     username: req.body.username,
@@ -16,6 +17,28 @@ router.post("/register", async (req, res) => {
   try {
     const savedUser = await newUser.save();
     res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+//login
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.body.username });
+    !user && res.status(401).json("Wrong credentials");
+
+    const hashedPassword = CryptoJS.AES.decrypt(
+      user.password,
+      process.env.PASS_SECRET
+    );
+    const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+
+    OriginalPassword !== req.body.password && res.status(401).json("wrong credentials");
+    
+    const {password, ... others} = user._doc;
+
+    res.status(200).json(others);
   } catch (err) {
     res.status(500).json(err);
   }
