1882112778498882b0cb69120c7771b4ca60745c
Solomon Talemwa <adamtblack96@gmail.com>
2021-12-17 13:23:45 +0300

    set up auth function

---
+++ routes/auth.js
@@ -0,0 +1,24 @@
+import express from "express";
+import User from "../models/User.js";
+import CryptoJS from "crypto-js";
+
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.PASS_SECRET
+    ).toString(),
+  });
+  try {
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+export default router;
