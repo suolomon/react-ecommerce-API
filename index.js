f75259d71891523b842ba4be38fae99c27732b9e
Solomon Talemwa <adamtblack96@gmail.com>
2022-01-07 02:02:41 +0300

    add stripe route

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import express from "express";
 const app = express();
+import cors from "cors";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoute from "./routes/user.js";
 import authRoute from "./routes/auth.js";
 import productRoute from "./routes/product.js";
+import stripeRoute from "./routes/stripe.js";
 
 dotenv.config();
 
@@ -13,11 +15,14 @@ mongoose
   .then(() => console.log("DB connection Successful!"))
   .catch((err) => console.log(err));
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/auth", authRoute);
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () =>
   console.log("Server running on PORT 5000")
 );
+
---
+++ routes/stripe.js
@@ -0,0 +1,26 @@
+import express from "express";
+
+const router = express.Router();
+
+import stripe from "stripe";
+
+process.env.STRIPE_SECRET_KEY;
+
+router.post("/payment", (req, res) => {
+  stripe.charges.create(
+    {
+      source: req.body.tokenId,
+      amount: req.body.amount,
+      currency: "ugx",
+    },
+    (stripeErr, stripeRes) => {
+      if (stripeErr) {
+        res.status(500).json(stripeErr);
+      } else {
+        res.status(200).json(stripeRes);
+      }
+    }
+  );
+});
+
+export default router;
